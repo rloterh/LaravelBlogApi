beee4fef1f5826682e370d34aec73f5729864429
rloterh <rloterh@gmail.com>
2023-07-31 11:22:40 +0000

    Update App.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="bg-gray-100 min-h-screen">
+    <nav class="bg-blue-600 text-white p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <router-link to="/" class="text-xl font-semibold">Blog App</router-link>
+        <div>
+          <router-link to="/login" class="mr-4">Login</router-link>
+          <router-link to="/register">Register</router-link>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mx-auto mt-4">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* */
+</style>
