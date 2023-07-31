f1c28c10285a6e39302b32e93645d37942f3dddc
rloterh <rloterh@gmail.com>
2023-07-31 13:16:51 +0000

    Update index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your Vue.js App</title>
+</head>
+
+<body>
+    <!-- Your Vue.js app will be mounted inside this div -->
+    <div id="app"></div>
+
+    <!-- Include the main.js file (not app.js) -->
+    <script src="{{ mix('js/main.js') }}"></script>
+</body>
+
+</html>
