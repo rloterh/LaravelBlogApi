f9d781b463073ceee715772fe757164d07ebf564
rloterh <rloterh@gmail.com>
2023-07-31 09:13:17 +0000

    Configure api routes

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Authentication Routes
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected Routes (Requires JWT Token)
+Route::group(['middleware' => 'auth:api'], function () {
+    // Post Routes
+    Route::get('posts', 'PostController@index');
+    Route::post('posts', 'PostController@store');
+    Route::get('posts/{id}', 'PostController@show');
+    Route::put('posts/{id}', 'PostController@update');
+    Route::delete('posts/{id}', 'PostController@destroy');
+
+    // Like and Comment Routes
+    Route::post('posts/{id}/like', 'PostController@like');
+    Route::post('posts/{id}/comment', 'PostController@comment');
 });
