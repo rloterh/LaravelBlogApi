cf85cab10f1a23f7112423cfe6e0c79b1795f5b8
rloterh <rloterh@gmail.com>
2023-07-31 09:29:16 +0000

    Update and validate the Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $fillable = ['title', 'description', 'image'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'post_likes', 'post_id', 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // Validation rules for creating/updating a Post
+    public static function validationRules()
+    {
+        return [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ];
+    }
 }
