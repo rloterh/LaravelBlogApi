b316d1d5d4e6e690a19140d8e6b1e2be23a8cde4
rloterh <rloterh@gmail.com>
2023-07-31 11:40:00 +0000

    Update CRUD pages

---
+++ resources/js/components/posts/EditPost.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="container mx-auto py-16">
+      <div class="max-w-md mx-auto bg-white shadow p-8 rounded-lg">
+        <h2 class="text-3xl font-semibold text-center mb-6">Edit Post</h2>
+        <form @submit.prevent="updatePost">
+          <div class="mb-4">
+            <label for="title" class="block font-semibold">Title</label>
+            <input v-model.trim="title" type="text" id="title" name="title" class="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring focus:border-blue-500" required>
+          </div>
+          <div class="mb-4">
+            <label for="description" class="block font-semibold">Description</label>
+            <textarea v-model.trim="description" id="description" name="description" rows="4" class="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring focus:border-blue-500" required></textarea>
+          </div>
+          <div class="mb-4">
+            <label for="image" class="block font-semibold">Image URL</label>
+            <input v-model.trim="image" type="url" id="image" name="image" class="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring focus:border-blue-500" required>
+          </div>
+          <div>
+            <button type="submit" class="w-full bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditPost',
+  data() {
+    return {
+      title: '', // Initialize with existing post title
+      description: '', // Initialize with existing post description
+      image: '', // Initialize with existing post image URL
+    };
+  },
+  methods: {
+    updatePost() {
+      // Your update post logic here
+      // You can make an API request to update the post using the entered data
+    },
+  },
+};
+</script>
+
+<style>
+/* */
+</style>
---
+++ resources/js/components/posts/PostDetails.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="container mx-auto py-16">
+      <div class="max-w-md mx-auto bg-white shadow p-8 rounded-lg">
+        <h2 class="text-3xl font-semibold mb-4">{{ post.title }}</h2>
+        <img :src="post.image" alt="Post Image" class="w-full rounded-lg mb-4">
+        <p class="text-gray-600 mb-6">{{ post.description }}</p>
+        <div class="flex items-center justify-between">
+          <div class="text-blue-600 font-semibold">Likes: {{ post.likes }}</div>
+          <button @click="likePost" :disabled="post.liked" class="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700" v-if="userLoggedIn">Like</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostDetails',
+  data() {
+    return {
+      post: {
+        title: 'Post Title', // Placeholder for post title
+        image: 'https://via.placeholder.com/400x300', // Placeholder for post image URL
+        description: 'Post description goes here', // Placeholder for post description
+        likes: 0, // Placeholder for number of likes
+        liked: false, // Placeholder for checking if the user has already liked the post
+      },
+      userLoggedIn: true, // Set this to true/false based on user authentication status
+    };
+  },
+  methods: {
+    likePost() {
+      // Your like post logic here
+      // You can make an API request to like the post and update the number of likes and liked status
+    },
+  },
+};
+</script>
+
+<style>
+/* */
+</style>
