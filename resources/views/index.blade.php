157bef968a6ccdc454b8e8657a11a7b8e7da3eb8
rloterh <rloterh@gmail.com>
2023-08-01 15:27:50 +0000

    Update frontend configuration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog App</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    
 </head>
 
 <body>
     <!-- Your Vue.js app will be mounted inside this div -->
     <div id="app"></div>
-
-    <!-- Include the main.js file (not app.js) -->
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
