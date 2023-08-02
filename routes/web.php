790ae3d91a59ab19cee4b67aa1371b01203a4258
rloterh <rloterh@gmail.com>
2023-08-02 12:54:44 +0000

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '.*');
+// Homepage
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+// Registration
+Route::get('/register', 'AuthController@showRegistrationForm')->name('register');
+Route::post('/register', 'AuthController@register');
+
+// Login
+Route::get('/login', 'AuthController@showLoginForm')->name('login');
+Route::post('/login', 'AuthController@login');
+
+// Logout
+Route::post('/logout', 'AuthController@logout')->name('logout');
+
+// All Posts
+Route::get('/posts', 'PostController@index')->name('all_posts');
+
+// Post Details
+Route::get('/posts/{id}', 'PostController@show')->name('post_details');
+
+// Create Post
+Route::get('/posts/create', 'PostController@create')->name('create_post');
+Route::post('/posts', 'PostController@store');
+
+// Edit Post
+Route::get('/posts/{id}/edit', 'PostController@edit')->name('edit_post');
+Route::put('/posts/{id}', 'PostController@update');
+
+// Delete Post
+Route::delete('/posts/{id}', 'PostController@destroy')->name('delete_post');
