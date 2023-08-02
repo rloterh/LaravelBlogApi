b3da859ee516099471e00530a39ffb7d63736e93
rloterh <rloterh@gmail.com>
2023-08-02 12:53:56 +0000

    Remove Vue view files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,1 @@
 import './bootstrap';
-import Vue from 'vue';
-import App from './components/App.vue';
-import router from './router';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
