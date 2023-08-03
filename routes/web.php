0f14a7e18a5651c99826a8b98faab4b54d17b187
rloterh <rloterh@gmail.com>
2023-08-03 17:05:32 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
-// Redirect to login if not authenticated
-Route::middleware('guest')->group(function () {
-    Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
-});
+// Public routes
+Route::get('/', function () { return view('welcome'); });
+Route::get('/')->name('home')->middleware('guest');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register')->middleware('guest');
+Route::post('/register', [AuthController::class, 'register']);
 
-// Routes accessible only to authenticated users
+// Protected routes
 Route::middleware('auth')->group(function () {
-    // Logout route
+    Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    // Routes related to posts
-    Route::resource('posts', PostController::class);
+    // Post routes
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/posts/{id}/like', [PostController::class, 'like'])->name('posts.like');
+    Route::post('/posts/{id}/comment', [PostController::class, 'comment'])->name('posts.comment');
 });
 
-// Redirect to login if not authenticated, else display posts
-Route::get('/welcome', [PostController::class, 'welcome'])->name('welcome');
-
-// Registration
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+// Redirect to login page for all other routes
+Route::fallback(function () {
+    return redirect()->route('login');
+});
