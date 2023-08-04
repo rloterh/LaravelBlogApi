5986bf14e1dddba41ac32b750af213035e0c3cc7
rloterh <rloterh@gmail.com>
2023-08-04 06:48:26 +0000

    Update api.php to reference controller full path

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Authentication Routes
-Route::post('register', 'AuthController@register');
-Route::post('login', 'AuthController@login');
+Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
+Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
 
-// Protected Routes (Requires JWT Token)
-Route::group(['middleware' => 'auth:api'], function () {
-    // Post Routes
-    Route::get('posts', 'PostController@index');
-    Route::post('posts', 'PostController@store');
-    Route::get('posts/{id}', 'PostController@show');
-    Route::put('posts/{id}', 'PostController@update');
-    Route::delete('posts/{id}', 'PostController@destroy');
+// Protected routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('/dashboard', 'App\Http\Controllers\API\AuthController@dashboard');
+    Route::post('/logout', 'App\Http\Controllers\API\AuthController@logout');
 
-    // Like and Comment Routes
-    Route::post('posts/{id}/like', 'PostController@like');
-    Route::post('posts/{id}/comment', 'PostController@comment');
+    // Post routes
+    Route::get('/posts', 'App\Http\Controllers\API\PostController@index');
+    Route::get('/posts/{id}', 'App\Http\Controllers\API\PostController@show');
+    Route::post('/posts', 'App\Http\Controllers\API\PostController@store');
+    Route::put('/posts/{id}', 'App\Http\Controllers\API\PostController@update');
+    Route::delete('/posts/{id}', 'App\Http\Controllers\API\PostController@destroy');
+    Route::post('/posts/{id}/like', 'App\Http\Controllers\API\PostController@like');
+    Route::post('/posts/{id}/comment', 'App\Http\Controllers\API\PostController@comment');
+});
+
+// Redirect to login page for all other routes
+Route::fallback(function () {
+    return response()->json(['error' => 'Route not found'], 404);
 });
