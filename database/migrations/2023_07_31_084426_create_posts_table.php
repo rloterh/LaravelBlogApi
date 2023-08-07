24cd67d6fa50a2941a925731ba75b1fcf2663024
rloterh <rloterh@gmail.com>
2023-08-07 06:39:53 +0000

    Update Posts migration file

--- database/migrations/2023_07_31_084426_create_posts_table.php
+++ database/migrations/2023_07_31_084426_create_posts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('description');
             $table->string('image')->nullable();
