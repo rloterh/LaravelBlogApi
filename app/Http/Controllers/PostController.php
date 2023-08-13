f5c7ef60b0e71ac9fa584b4e48c652702cedc8de
rloterh <rloterh@gmail.com>
2023-08-13 21:55:09 +0000

    Refactor PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,153 +4,86 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
-use App\Models\Comment;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
-class PostController extends Controller
-{
-    public function welcome()
-    {
-        if (!Auth::check()) {
-            return redirect()->route('login');
-        }
-
-        $posts = Post::all();
-
-        return view('welcome', compact('posts'));
-    }
-    
-    public function index()
-    {
-        $posts = Post::all();
-        return response()->json($posts, 200);
+class PostController extends Controller {
+    public function index() {
+        $posts = Post::with(['user', 'category', 'likes', 'comments.user'])->orderBy('created_at', 'desc')->get();
+        return response()->json(['posts' => $posts]);
     }
 
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $validator = Validator::make($request->all(), [
             'title' => 'required|string',
             'description' => 'required|string',
+            'image' => 'required|image',
+            'category_id' => 'required|exists:categories,id',
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
+            return response()->json(['errors' => $validator->errors()], 400);
         }
 
-        // Upload the image and get its URL
-        //$imagePath = $request->file('image')->store('public/images');
-        $imageUrl = null;
-        if ($request->has('image')) {
-            $imageUrl = $request->image;
-        }
+        $imagePath = $request->file('image')->store('posts', 'public');
 
-        $post = Post::create([
+        $post = Auth::user()->posts()->create([
             'title' => $request->title,
             'description' => $request->description,
-            'image' => $imageUrl,
-            'user_id' => auth()->user()->id,
+            'image' => $imagePath,
+            'category_id' => $request->category_id,
         ]);
 
-        return response()->json($post, 201);
+        return response()->json(['message' => 'Post created successfully', 'post' => $post], 201);
     }
 
-    public function getPostUser($postId)
-    {
-        $post = Post::findOrFail($postId);
-        $user = $post->user; // Accessing the user relationship
-        
-        return response()->json([
-            'post' => $post,
-            'user' => $user,
-        ]);
-    }
-
-    public function show($id)
-    {
-        $post = Post::findOrFail($id);
-        return response()->json($post, 200);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $post = Post::findOrFail($id);
-
-        // Ensure the authenticated user owns the post
-        // if (auth()->user()->id !== $post->user_id) {
-        //     return response()->json(['error' => 'Unauthorized'], 401);
-        // }
-
+    public function update(Request $request, Post $post) {
+        $this->authorize('update', $post);
+    
         $validator = Validator::make($request->all(), [
             'title' => 'required|string',
             'description' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'image' => 'nullable|image', // Allow updating the image, but it's optional
         ]);
-
+    
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
+            return response()->json(['errors' => $validator->errors()], 400);
         }
-
-        $post = Post::findOrFail($id);
-
-        // If a new image is uploaded, update the image URL
+    
+        $postData = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'category_id' => $request->category_id,
+        ];
+    
         if ($request->hasFile('image')) {
-            // $imagePath = $request->file('image')->store('public/images');
-            // $imageUrl = asset('storage/images/' . basename($imagePath));
-            $post->image = $imageUrl;
+            // Delete the old image
+            Storage::disk('public')->delete($post->image);
+            
+            // Store the new image
+            $imagePath = $request->file('image')->store('posts', 'public');
+            $postData['image'] = $imagePath;
         }
-
-        $post->title = $request->title;
-        $post->description = $request->description;
-        $post->save();
-
-        return response()->json($post, 200);
+    
+        $post->update($postData);
+    
+        return response()->json(['message' => 'Post updated successfully', 'post' => $post]);
     }
 
-    public function destroy($id)
-    {
-        $post = Post::findOrFail($id);
+    public function destroy(Post $post) {
+        $this->authorize('delete', $post);
 
-        // Ensure the authenticated user owns the post
-        // if (auth()->user()->id !== $post->user_id) {
-        //     return response()->json(['error' => 'Unauthorized'], 401);
-        // }
-
+        Storage::disk('public')->delete($post->image);
         $post->delete();
-        return response()->json(['message' => 'Post deleted successfully'], 200);
+
+        return response()->json(['message' => 'Post deleted successfully']);
     }
 
-    public function like(Request $request, $id)
-    {
-        $post = Post::findOrFail($id);
-        $user = auth()->user();
-
-        // Check if the user has already liked the post
-        if ($post->likes()->where('user_id', $user->id)->exists()) {
-            return response()->json(['error' => 'User has already liked the post'], 400);
-        }
-
-        $post->likes()->attach($user->id);
-        return response()->json(['message' => 'Post liked successfully'], 200);
-    }
-
-    public function comment(Request $request, $id)
-    {
-        $validator = Validator::make($request->all(), [
-            'content' => 'required|string',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
-        }
-
-        $post = Post::findOrFail($id);
-        $user = auth()->user();
-
-        $comment = new Comment([
-            'content' => $request->content,
-            'user_id' => $user->id,
-        ]);
-
-        $post->comments()->save($comment);
-        return response()->json(['message' => 'Comment added successfully'], 201);
+    public function like(Post $post) {
+        Auth::user()->likes()->toggle($post);
+        return response()->json(['message' => 'Like updated successfully']);
     }
 }
